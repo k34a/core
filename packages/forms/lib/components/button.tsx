02a7e480785f3d783a02abbac9c9c2505ade386e
Saksham Garg <Saksham2700@gmail.com>
2025-10-24 17:34:51 +0530

    fixes

--- packages/forms/lib/components/button.tsx
+++ packages/forms/lib/components/button.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import React from "react";
+import { Button as MantineBtn } from "@mantine/core";
 
 export function Button(props: React.ButtonHTMLAttributes<HTMLButtonElement>) {
-  return <button {...props} />;
+  return <MantineBtn {...props} />;
 }
